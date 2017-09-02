d9933cafa76155fc6bb5d2d43cb9e13bf4391eb2
IshanArya <ishan.arya130@gmail.com>
2017-09-02 19:25:55 -0400

    Currently an anime information app

--- app/js/main.js
+++ app/js/main.js
@@ -3,10 +3,23 @@ $(function() {
 	let $response;
 	let formattedShows = [];
 
+	let $results = $('#results');
+	let elements = {
+		$result: $('<a href=""><div class="row result"><div class="three columns"><img src=""></div><div class="nine columns"><h3 class="title"></h3><div class="genres"></div><h5 class="views"></h5><div class="otherNames"></div><p class="description"></p></div></div></a>'),
+		$complete: $('<i class="fa fa-check-circle green" title="Completed" aria-hidden="true"></i>'),
+		$ongoing: $('<i class="fa fa-times-circle blue" title="Ongoing" aria-hidden="true"></i>'),
+		$genre: $('<span class="genre"></span>'),
+		$otherName: $('<h6 class="otherName"></h6>'),
+		$bulletOtherName: $('<h6 class="otherName">&#9643;</h6>'),
+		$breakline: $('<hr class="breakline">')
+	}
+
 	$('#searchForm').submit((e) => {
 		e.preventDefault();
 		let search = $.trim($('#search').val());
 		if(search.length > 1) {
+			$results.empty();
+			$('#search').val('');
 			$.ajax({
 				type: "POST",
 				url: "http://kissanime.ru/Search/SearchSuggestx",
@@ -50,13 +63,13 @@ $(function() {
 								let statusAndViews;
 								if(thePs[2].textContent.includes("Date")) {
 									statusAndViews =  thePs[3].textContent;
-									info.summary = thePs[5].textContent;
+									info.description = thePs[5].textContent;
 								} else {
 									statusAndViews = thePs[2].textContent;
-									info.summary = thePs[4].textContent;
+									info.description = thePs[4].textContent;
 								}
 
-								info.completed = statusAndViews[statusAndViews.indexOf("Status") + 8] === "C";
+								info.complete = statusAndViews[statusAndViews.indexOf("Status") + 8] === "C";
 
 								let viewsIndex = statusAndViews.indexOf("Views");
 								let views = statusAndViews.substring(viewsIndex + 7, viewsIndex + 20);
@@ -66,8 +79,39 @@ $(function() {
 								info.image = $animePage.find('.barContent')[3].getElementsByTagName('img')[0].getAttribute('src');
 
 
-								console.log(info);
+								// console.log(info);
 								formattedShows.push(info);
+
+								let $entry = elements.$result.clone();
+								$entry.attr("href", info.url);
+
+								$entry.find('img').attr('src', info.image);
+
+								info.complete ? $entry.find('.title').html(info.name + elements.$complete.html()) : $entry.find('.title').html(info.name + elements.$ongoing.html());
+
+								for(let x = 0; x < info.genres.length; x++) {
+									let $tempGenre = elements.$genre.clone();
+									$tempGenre.text(info.genres[x]);
+									$entry.find('.genres').append($tempGenre);
+								}
+
+								$entry.find('.views').text(info.views + " views");
+
+								for(let x = 0; x < info.otherNames.length; x++) {
+									let $tempGenre = elements.$otherName.clone();
+									$tempGenre.text(info.otherNames[x]);
+									$entry.find('.otherNames').append($tempGenre);
+									if(x != info.otherNames.length - 1) {
+									 $entry.find('.otherNames').append(elements.$bulletOtherName.clone());
+									}
+								}
+
+								info.description.length > 270 ? $entry.find('.description').text(info.description.substring(0, 300) + "...") : $entry.find('.description').text(info.description);
+								$entry.find('.description').attr("title", info.description);
+
+
+								$results.append($entry);
+								$results.append(elements.$breakline.clone());
 							}
 						});
 
