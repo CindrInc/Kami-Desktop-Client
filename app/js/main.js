18b94745f5df8556898d5a693a5205eec5763d3c
IshanArya <ishan.arya130@gmail.com>
2017-09-02 13:41:04 -0400

    Got information for each show

--- app/js/main.js
+++ app/js/main.js
@@ -1,62 +1,75 @@
 $(function() {
 
-	var $response;
-	var formattedShows = [];
+	let $response;
+	let formattedShows = [];
 
 	$('#searchForm').submit((e) => {
 		e.preventDefault();
-		var search = $.trim($('#search').val());
+		let search = $.trim($('#search').val());
 		if(search.length > 1) {
 			$.ajax({
 				type: "POST",
-				url: "http://kissanime.ru/Search/Anime",
+				url: "http://kissanime.ru/Search/SearchSuggestx",
 				data: "type=Anime" + '&keyword=' + search,
 				success: function (message) {
 					$response = $(message);
-					console.log(message);
-					// console.log($response);
-					// $('body').append($response);
-					var shows = $response.find('tr');
-					shows.each(function(i, show) {
-						if(i > 1) {
-							var info = {};
-							var subelements = show.children;
-							if(subelements[0].children.length == 3) {
-								info.updated = true;
-								info.popular = true;
-							} else if(subelements[0].children.length == 2) {
-								if(subelements[0].children[1].textContent.includes("update")) {
-									info.updated = true;
-									info.popular = false;
-								} else {
-									info.updated = false;
-									info.popular = true;
+					// console.log(message);
+					
+					for(let i = 0; i < $response.length; i++) {
+						let info = {};
+
+						info.url = $response[i].getAttribute('href');
+
+						let $animePage;
+						$.ajax({
+							url: info.url,
+							success: function(data) {
+								// console.log(data);
+								$animePage = $(data);
+								let infoDiv = $animePage.find('.barContent')[0].getElementsByTagName('div')[1];
+
+								info.name = infoDiv.getElementsByTagName('a')[0].textContent;
+
+
+
+								let thePs = infoDiv.getElementsByTagName('p');
+
+
+								info.otherNames = [];
+								for(let x = 0; x < thePs[0].getElementsByTagName('a').length; x++) {
+									info.otherNames.push(thePs[0].getElementsByTagName('a')[x].textContent);
 								}
-							} else {
-								info.updated = false;
-								info.popular = false;
+
+
+								info.genres = [];
+								for(let x = 0; x < thePs[1].getElementsByTagName('a').length; x++) {
+									info.genres.push(thePs[1].getElementsByTagName('a')[x].textContent);
+								}
+
+
+								let statusAndViews;
+								if(thePs[2].textContent.includes("Date")) {
+									statusAndViews =  thePs[3].textContent;
+									info.summary = thePs[5].textContent;
+								} else {
+									statusAndViews = thePs[2].textContent;
+									info.summary = thePs[4].textContent;
+								}
+
+								info.completed = statusAndViews[statusAndViews.indexOf("Status") + 8] === "C";
+
+								let viewsIndex = statusAndViews.indexOf("Views");
+								let views = statusAndViews.substring(viewsIndex + 7, viewsIndex + 20);
+								views = views.replace(/,/g, "");
+								info.views = parseInt(views, 10);
+
+
+								console.log(info);
+								formattedShows.push(info);
 							}
-							info.name = subelements[0].children[0].textContent;
-							info.link = subelements[0].children[0].getAttribute("href");
+						});
 
-							console.log($response.find('a[href="' + info.link + '"]').length);
-							// info.description = $response.find('a[href="' + info.link + '"]')[1].parentElement.children[1].textContent;
-							// info.image = $response.find('a[href="' + info.link + '"]')[1].parentElement.parentElement.children[0].getAttribute('src');
-
-							if(subelements[1].children.length > 0) {
-								info.completed = false;
-								info.latestEpisode = {};
-								info.latestEpisode.number = parseInt(subelements[1].children[0].textContent.substring(8), 10);
-								info.latestEpisode.link = subelements[1].children[0].getAttribute("href");
-							} else {
-								info.completed = true;
-							}
-
-							// console.log(info);
-							formattedShows.push(info);
-						}
-					});
-					// console.log(formattedShows);
+					}
 				}
 			});
 		}
